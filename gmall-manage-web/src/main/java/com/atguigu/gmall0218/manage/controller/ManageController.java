ca4764fcbf7837beb5f6c28d9cb58129dc4132d7
rcn <534897552rqc@gmail.com>
2020-08-05 23:23:40 +0800

    第三天前五节，搭建后台模块，

---
+++ gmall-manage-web/src/main/java/com/atguigu/gmall0218/manage/controller/ManageController.java
@@ -0,0 +1,18 @@
+package com.atguigu.gmall0218.manage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ManageController {
+
+
+    @RequestMapping("getCatalog1")
+    @ResponseBody
+    public void getCatalog1(){
+
+    }
+
+
+}
---
+++ gmall-manage-web/src/test/java/com/atguigu/gmall0218/manage/GmallManageWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall0218.manage;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallManageWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
